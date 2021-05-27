305ada9743f034ebdb09f79a0d43d0cc6fe606a8
amartial <ambe@bnb-investment.com>
2021-05-27 10:29:34 +0100

    Le script traite les buckets passés en parametre dans inputBuckets

--- bucket-encryption.py
+++ bucket-encryption.py
@@ -11,22 +11,33 @@ s3resource = boto3.resource('s3')
 
 response = s3client.list_buckets()
 
-for bucket in response['Buckets']:
-  print('bucket: ', bucket['Name'])
+inputBuckets = [
+  'my-s3-data',
+  'martial-bucket',
+  'fastapistore',
+  'testbucketobject0001',
+  'my-empty-bucket-001',
+  'unexistingbucket'
+]
+
+for bucket in inputBuckets:
+  print('bucket: ', bucket)
   try:
-    enc = s3client.get_bucket_encryption(Bucket=bucket['Name'])
+    enc = s3client.get_bucket_encryption(Bucket=bucket)
     rules = enc['ServerSideEncryptionConfiguration']['Rules']
     print('Bucket Encrypted: %s' % (rules))
 
   except ClientError as e:
+    if e.response['Error']['Code'] == 'NoSuchBucket':
+      print('Bucket not found: ', bucket)
     if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-      print('Bucket: %s, no server-side encryption' % (bucket['Name']))
+      print('Bucket: %s, no server-side encryption' % (bucket))
       try:
-        response = s3client.get_bucket_encryption(Bucket=bucket['Name'])
+        response = s3client.get_bucket_encryption(Bucket=bucket)
         print('res', response)
       except s3client.exceptions.ClientError as e2:
           if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-            response = s3client.put_bucket_encryption(Bucket=bucket['Name'], ServerSideEncryptionConfiguration={'Rules': [{'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}},]})
+            response = s3client.put_bucket_encryption(Bucket=bucket, ServerSideEncryptionConfiguration={'Rules': [{'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}},]})
           else:
               print("Unexpected error: %s" % e2)
     else:
