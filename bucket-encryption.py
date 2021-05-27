7a38a9c936bb7753c8ea3b7bb8e46801fadd6a89
amartial <martialmbe@gmail.com>
2021-05-27 15:01:15 +0100

    undo to initial version

--- bucket-encryption.py
+++ bucket-encryption.py
@@ -9,35 +9,24 @@ import sys
 s3client = boto3.client('s3')
 s3resource = boto3.resource('s3')
 
-# response = s3client.list_buckets()
+response = s3client.list_buckets()
 
-inputBuckets = [
-  'my-s3-data',
-  'martial-bucket',
-  'fastapistore',
-  'testbucketobject0001',
-  'my-empty-bucket-001',
-  'unexistingbucket'
-]
-
-for bucket in inputBuckets:
-  print('bucket: ', bucket)
+for bucket in response['Buckets']:
+  print('bucket: ', bucket['Name'])
   try:
-    enc = s3client.get_bucket_encryption(Bucket=bucket)
+    enc = s3client.get_bucket_encryption(Bucket=bucket['Name'])
     rules = enc['ServerSideEncryptionConfiguration']['Rules']
     print('Bucket Encrypted: %s' % (rules))
 
   except ClientError as e:
-    if e.response['Error']['Code'] == 'NoSuchBucket':
-      print('Bucket not found: ', bucket)
-    elif e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-      print('Encrypting bucket: %s' % (bucket))
+    if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+      print('Bucket: %s, no server-side encryption' % (bucket['Name']))
       try:
-        response = s3client.get_bucket_encryption(Bucket=bucket)
+        response = s3client.get_bucket_encryption(Bucket=bucket['Name'])
         print('res', response)
       except s3client.exceptions.ClientError as e2:
           if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-            response = s3client.put_bucket_encryption(Bucket=bucket, ServerSideEncryptionConfiguration={'Rules': [{'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}},]})
+            response = s3client.put_bucket_encryption(Bucket=bucket['Name'], ServerSideEncryptionConfiguration={'Rules': [{'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}},]})
           else:
               print("Unexpected error: %s" % e2)
     else:
--- s3-objects-encryption.py
+++ s3-objects-encryption.py
@@ -12,39 +12,29 @@ import sys
 s3client = boto3.client('s3')
 s3resource = boto3.resource('s3')
 
-# response = s3client.list_buckets()
+response = s3client.list_buckets()
 
-# Lister les buckets qui seront pris en compte lors de l'execution
-inputBuckets = [
-  'my-s3-data',
-  'martial-bucket',
-  'fastapistore',
-  'testbucketobject0001',
-  'my-empty-bucket-001',
-  'unexistingbucket'
-]
-
-for bucket in inputBuckets:
-  print('bucket: ', bucket)
+for bucket in response['Buckets']:
+  print('bucket: ', bucket['Name'])
   try:
-    enc = s3client.get_bucket_encryption(Bucket=bucket)
+    enc = s3client.get_bucket_encryption(Bucket=bucket['Name'])
     rules = enc['ServerSideEncryptionConfiguration']['Rules']
     print('Bucket Encrypted: %s' % (rules))
-    bucketResource = s3resource.Bucket(bucket)
+    bucketResource = s3resource.Bucket(bucket['Name'])
     # print('bucketResource', bucketResource)
     i = 0
     for objectSummary in bucketResource.objects.all():
-        objectResource = s3resource.Object(bucket, objectSummary.key)
+        objectResource = s3resource.Object(bucket['Name'], objectSummary.key)
         # print('encryption: ', objectResource.key, ' clef: ', objectResource.server_side_encryption, ' kms_key_id: ', objectResource.ssekms_key_i**************.*;:server_side_encryption is None:
         if objectResource.server_side_encryption is None:
             print("Cryptage de l'objet: ", objectResource.key)
             copy_source = {
-                'Bucket': bucket,
+                'Bucket': bucket['Name'],
                 'Key': objectResource.key
             }
 
             resp = s3client.copy_object(
-                Bucket=bucket,
+                Bucket=bucket['Name'],
                 CopySource=copy_source,
                 Key=objectResource.key,
                 ServerSideEncryption='aws:kms'
@@ -59,9 +49,7 @@ for bucket in inputBuckets:
         # encrypt_file("s3://" + bucket['Name'] + "/" + my_file.key, my_kms_key_id)
 
   except ClientError as e:
-    if e.response['Error']['Code'] == 'NoSuchBucket':
-      print('Bucket not found: ', bucket)
-    elif e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+    if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
     #   print('Bucket: %s, no server-side encryption' % (bucket['Name']))
       continue
     else:
