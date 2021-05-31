0ad1f73b5fb14f78337ce8ba5eda1be4982754ef
amartial <martialmbe@gmail.com>
2021-05-31 13:54:54 +0100

    Ajustement copy file

--- s3-objects-copy.py
+++ s3-objects-copy.py
@@ -17,7 +17,7 @@ parser.add_argument('--debug', action='store_true', dest='debug', default=False)
 
 args = parser.parse_args()
 
-print('args', args, args.bucket_source)
+print('args', args)
 
 # Set up Debug Logging
 if args.debug:
@@ -42,7 +42,7 @@ if not args.key_dest:
     error = True
 
 if error:
-    print('s3-objects-copy.py --bucket-source=bucket_source --key-source=key_source --bucket-dest=bucket_dest --key-dest=key_dest')
+    print('python s3-objects-copy.py --bucket-source=bucket_source --key-source=key_source --bucket-dest=bucket_dest --key-dest=key_dest')
 
 if args.bucket_source and args.key_source and args.bucket_dest and args.key_dest:
     print('Executing s3 copy ...')
@@ -51,11 +51,22 @@ if args.bucket_source and args.key_source and args.bucket_dest and args.key_dest
         'Key': args.key_source
     }
     try:
-        s3resource.meta.client.copy(copy_source, args.bucket_dest, args.key_dest)
-        print('Succes copy file')
+      source_list = args.key_source.split('/')
+      dest_list = args.key_dest.split('/')
+      key_list = dest_list[:-1] + source_list
+      key_string = '/'.join(key_list)
+      print('destination: ', key_string)
+      # s3resource.meta.client.copy(copy_source, args.bucket_dest, key_string)
+      copy_source = {
+        'Bucket': args.bucket_source,
+        'Key': args.key_source
+      }
+      bucket = s3resource.Bucket(args.bucket_dest)
+      bucket.copy(copy_source, key_string)
+      print('Succes copy file')
 
     except ClientError as e:
         if e.response['Error']['Code'] == 'NoSuchBucket':
-            print('Bucket not found: ', bucket)
+            print('Bucket not found: ')
         else:
             print("Unexpected error: %s" % e)
